acabbaf633c6f82558ae9d0f3228e9b6989aaa18
Vanessa-Macleine <vmacleine@gmail.com>
2026-04-28 17:27:28 -0300

    inicio-cadastro-funcionarios

---
+++ funcionarios.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Chamados - Funcionários</title>
+    <link rel="stylesheet" href="assets/css/funcionario.css">
+</head>
+
+<body>
+
+    <div class="botoes">
+        <a href="index.php">Iniciar</a>
+        <a href="funcionarios-listar.php">Listar</a>
+
+    </div>
+    <div class="container">
+        <h1>Cadastro de Funcionários</h1>
+
+
+        <form action="backend/funcionarios-cadastrar.php" method="POST">
+            <div class="input-group">
+                <label for="nome">Nome</label>
+                <input type="text" name="nome" id="nome">
+            </div>
+            <div class="input-group">
+                <label for="email">E-mail</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <div class="input-group">
+                <label for="telefone">Telefone</label>
+                <input type="text" name="telefone" id="telefone">
+            </div>
+
+            <input type="submit" value="Cadastrar">
+        </form>
+    </div>
+
+    <script src="assets/js/inputmask.js"></script>
+
+    <script>
+        var telefone = document.getElementById('telefone');
+
+        var im = new Inputmask({
+            mask: [
+                "(99) 9999-9999", // fixo
+                "(99) 99999-9999" // celular
+            ],
+            keepStatic: true
+        });
+
+        im.mask(telefone);
+    </script>
+</body>
+
+
+</html>

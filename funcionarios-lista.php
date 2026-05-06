487c6cb2245c9f467dfcd63b3abf48f8c47d729b
Vanessa-Macleine <vmacleine@gmail.com>
2026-05-06 17:25:08 -0300

    funcionario-lista-inicio

---
+++ funcionarios-lista.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funcionarios - Lista</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/funcionario/funcionario-lista.css">
+</head>
+
+<body>
+    <?php require_once 'includes/header.php';
+    ?>
+    <main>
+        <h1 class="titulo-pagina">Funcionários</h1>
+        <div class="top-actions">
+
+            <div class="search-box">
+                <input type="text" placeholder="Buscar por nome">
+                <button class="btn buscar">Buscar</button>
+            </div>
+
+            <div class="action-buttons">
+                <a href="funcionarios-cadastrar.php" class="btn">Cadastrar</a>
+                <a href="index.php" class="btn outline">Início</a>
+            </div>
+
+        </div>
+        <div class="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Telefone</th>
+                        <th>Data de cadastro</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Maria Silva</td>
+                        <td>maria@email.com</td>
+                        <td>(11) 99999-9999</td>
+                        <td>01/01/2025</td>
+                        <td class="acoes">
+                            <a href="#" class="table-btn editar">Editar</a>
+                            <a href="#" class="table-btn status">Ativar/Inativar</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </main>
+
+</body>
+
+</html>

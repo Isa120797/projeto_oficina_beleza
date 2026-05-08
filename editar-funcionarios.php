238179f3a09d86960fcbf7fc3d4d939cf601b1e3
Vanessa-Macleine <vmacleine@gmail.com>
2026-05-08 16:26:00 -0300

    inicio-backend

--- editar-funcionarios.php
+++ editar-funcionarios.php
@@ -1,3 +1,40 @@
+<?php
+
+//conexao do PHP com o banco de dados MYSQL
+
+define('SERVIDOR', 'localhost');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('BANCO', 'db_oficina_beleza');
+
+//string de conexao usando PDO
+try {
+    $conexao = new PDO("mysql:host=" . SERVIDOR . ";dbname=" . BANCO . ";charset=utf8", USUARIO, SENHA);
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM tb_funcionario WHERE id=$id";
+
+    $comando = $conexao->prepare($sql);
+
+    $comando->execute();
+    // armazena em um array os funcionarios cadastrados no banco de dados
+
+    $funcionarios = $comando->fetchAll(PDO::FETCH_ASSOC);
+
+    // formata o código para exibição via var_dump
+    // echo "<pre>";
+    // var_dump($funcionarios);
+} catch (PDOException $erro) {
+
+    echo "Erro ao conectar no banco de dados" . $erro->getMessage();
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -17,18 +54,18 @@
         <h1>Atualizar Funcionários</h1>
 
 
-        <form action="backend/funcionarios-cadastrar.php" method="POST">
+        <form action="backend/atualizar-funcionarios.php?id=<?php echo $funcionarios[0]['id']; ?>" method="POST">
             <div class="input-group">
                 <label for="nome">Nome</label>
-                <input type="text" name="nome" id="nome">
+                <input value="<?php echo $funcionarios[0]['nome']; ?>" type="text" name="nome" id="nome">
             </div>
             <div class="input-group">
                 <label for="email">E-mail</label>
-                <input type="email" name="email" id="email">
+                <input value="<?php echo $funcionarios[0]['email']; ?>" type="email" name="email" id="email">
             </div>
             <div class="input-group">
                 <label for="telefone">Telefone</label>
-                <input type="text" name="telefone" id="telefone">
+                <input value="<?php echo $funcionarios[0]['telefone']; ?>" type="text" name="telefone" id="telefone">
             </div>
 
             <input type="submit" value="Cadastrar">

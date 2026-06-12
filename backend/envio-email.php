973b612c4ff6bf94bd6f379c6d1771bcadc1f325
Vanessa-Macleine <vmacleine@gmail.com>
2026-06-12 16:44:44 -0300

    nova-senha

--- backend/envio-email.php
+++ backend/envio-email.php
@@ -1,45 +1,63 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-require 'PHPMailer/src/Exception.php';
 
-$email = $_POST['email'];
+function enviaEmail($emailDestino, $token)
+{
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'ooficinadebeleza@gmail.com';                     //SMTP username
-    $mail->Password   = 'vkml jojg ayvj ljiw';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;
-    $mail->CharSet      = 'UTF-8';                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    try {
+        $mail = new PHPMailer(true);
 
-    //Recipients
-    $mail->setFrom('ooficinadebeleza@gmail.com', 'Oficina de Beleza');
-    $mail->addAddress($email);     //Add a recipient
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'ooficinadebeleza@gmail.com';                     //SMTP username
+        $mail->Password   = 'vkml jojg ayvj ljiw';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+        $mail->Port       = 587;
+        $mail->CharSet      = 'UTF-8';                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
+        //Recipients
+        $mail->setFrom('ooficinadebeleza@gmail.com', 'Oficina de Beleza');
+        $mail->addAddress($emailDestino);     //Add a recipient
 
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Recuperação de Senha';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    header('Location:../enviado_sucesso.php');
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        // Conteúdo
+        $mail->isHTML(true);
+        $mail->Subject = 'Recuperação de Senha';
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        $mail->Body = <<<HTML
+<body>
+    <h1>Olá, clique no link abaixo para cadastrar uma nova senha</h1>
+    <a href="http://localhost/projeto_cardapio/nova-senha.php?token=$token">Recuperar Senha</a>
+
+</body>
+
+    
+
+HTML;
+
+        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        $mail->AltBody = 'Email de cadastro de nova senha!';
+
+        // Enviar
+        if ($mail->send()) {
+            echo "E-mail enviado com sucesso!";
+        } else {
+            echo "Erro ao enviar: " . $mail->ErrorInfo;
+        }
+    } catch (Exception $e) {
+        echo "Erro: {$mail->ErrorInfo}";
+    }
 }
